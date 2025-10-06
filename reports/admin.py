a1e9147ad66c509167e5ee4f67d5b9bb1ab4f3a6
mostofa89 <mostofakamaljoy89@gmail.com>
2025-10-07 01:46:54 +0600

    Solve the design error of register

--- reports/admin.py
+++ reports/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Lost_reports
+from .models import Lost_reports, Found_reports, Claimed_items
 
 # Register your models here.
 admin.site.register(Lost_reports)
+admin.site.register(Found_reports)
+admin.site.register(Claimed_items)
