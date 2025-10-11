572cd1e94b9dc6a31773c1bb11df7ed161dbd627
mostofa89 <mostofakamaljoy89@gmail.com>
2025-10-11 15:57:29 +0600

    Modify Settings.py

--- Lost_and_Found/settings.py
+++ Lost_and_Found/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,6 +89,7 @@ DATABASES = {
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            
         }
     }
 }
